60e745ca1693997d6ffef10dd6889e7e1f471163
fred374 <fred.kelemen99@gmail.com>
2024-02-14 13:27:40 -0800

    Provided description for cards from ChatGPT

---
+++ app/src/main/java/com/example/tarot/CardInformationThree.java
@@ -0,0 +1,37 @@
+package com.example.tarot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CardInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_information);
+        Intent i = getIntent();
+        int id = i.getIntExtra("card_id", 0);
+
+        CardRepository repo = new CardRepository(getApplication());
+        repo.getCard(id).observe(this, card -> {
+            if (card != null) {
+                TextView name = findViewById(R.id.card_name);
+                name.setText(card.get(0).getName());
+
+                ImageView img = findViewById(R.id.card_image);
+                Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), card.get(0).getDraw());
+                img.setImageDrawable(drawable);
+
+                TextView description = findViewById(R.id.card_description);
+                description.setText(card.get(0).getDescription());
+            }
+        });
+
+    }
+}
