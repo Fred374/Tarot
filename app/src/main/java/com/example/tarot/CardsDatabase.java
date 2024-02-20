f9affd3413ca3af7453d33769ec6f75bf87a24f6
fred374 <fred.kelemen99@gmail.com>
2024-02-20 14:18:53 -0800

    updated database versions to account for new image ids

--- app/src/main/java/com/example/tarot/CardsDatabase.java
+++ app/src/main/java/com/example/tarot/CardsDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Card.class}, version = 7)
+@Database(entities = {Card.class}, version = 1)
 public abstract class CardsDatabase extends RoomDatabase {
 
     private static volatile CardsDatabase db;
