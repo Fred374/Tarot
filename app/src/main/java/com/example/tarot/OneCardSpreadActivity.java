0bcdc7b90d7c21c2acb88bdf9012bddd67e8827e
fred374 <fred.kelemen99@gmail.com>
2024-02-20 14:17:28 -0800

    Added appbar with navigation and info to spread activities

--- app/src/main/java/com/example/tarot/OneCardSpreadActivity.java
+++ app/src/main/java/com/example/tarot/OneCardSpreadActivity.java
@@ -3,12 +3,22 @@ package com.example.tarot;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+
+import java.util.Objects;
 
 public class OneCardSpreadActivity extends AppCompatActivity {
 
@@ -16,6 +26,11 @@ public class OneCardSpreadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_card_spread);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
         CardRepository repo = new CardRepository(getApplication());
         repo.getCard((int) (Math.random() * 78)).observe(this, cards -> {
             if (cards != null) {
@@ -30,6 +45,46 @@ public class OneCardSpreadActivity extends AppCompatActivity {
                 img.setImageDrawable(drawable);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        return super.onNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_window, null);
+                TextView txt = popupView.findViewById(R.id.info_text);
+                txt.setText(R.string.one_card_info);
+
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true;
+                final android.widget.PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                View view = findViewById(R.id.toolbar);
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onClick(View view) {
--- app/src/main/java/com/example/tarot/ThreeCardSpreadActivity.java
+++ app/src/main/java/com/example/tarot/ThreeCardSpreadActivity.java
@@ -3,14 +3,23 @@ package com.example.tarot;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
 import java.util.NavigableSet;
+import java.util.Objects;
 import java.util.TreeSet;
 
 public class ThreeCardSpreadActivity extends AppCompatActivity {
@@ -19,6 +28,10 @@ public class ThreeCardSpreadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three_card_spread);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         int[] vws = {R.id.card1, R.id.card2, R.id.card3};
         int[] txtVws = {R.id.card1_text, R.id.card2_text, R.id.card3_text};
@@ -44,6 +57,41 @@ public class ThreeCardSpreadActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_window, null);
+                TextView txt = popupView.findViewById(R.id.info_text);
+                txt.setText(R.string.three_card_info);
+
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true;
+                final android.widget.PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                View view = findViewById(R.id.toolbar);
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onClick(View view) {
